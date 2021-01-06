ea40f8702f4dd00670a435e48b17921b03e8e27f
Duxon900 <quintanojon@gmail.com>
2021-01-06 18:10:25 +0100

    initial commit

---
+++ app/src/main/java/ISAD1617/control/ui/MainUIKud.java
@@ -0,0 +1,45 @@
+package ISAD1617.control.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MainUIKud {
+
+    @FXML
+    private TableView<?> taula2;
+
+    @FXML
+    private TableColumn<?, ?> zut1Izena;
+
+    @FXML
+    private TableColumn<?, ?> zut1Abizena;
+
+    @FXML
+    private TableColumn<?, ?> zut1Kirola;
+
+    @FXML
+    private TableColumn<?, ?> zut1Urte;
+
+    @FXML
+    private TableColumn<?, ?> zut1Vegetta;
+
+    @FXML
+    private TableColumn<?, ?> zut1Argazki;
+
+    @FXML
+    private TableColumn<?, ?> zut2Izena;
+
+    @FXML
+    private TableColumn<?, ?> zut2Kirola;
+
+    @FXML
+    private TableColumn<?, ?> zut2Urte;
+
+    @FXML
+    private TableColumn<?, ?> zut2Vegetta777;
+
+    @FXML
+    private TableColumn<?, ?> zut2Argazki;
+
+}
